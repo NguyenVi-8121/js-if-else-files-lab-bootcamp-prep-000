7e4c7d8da40125c2efc51630b3019237b03d8a06
Vi Nguyen <nguyenvi.8121@gmail.com>
2017-10-25 21:32:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var name = "Tom"
+var name = "Susan"
 var height = "74"
 
 
@@ -7,7 +7,7 @@ module.exports = { name, height
 }
 
 if (name === "Susan") {
-  console/log("The name is correct")
+  console.log("The name is correct")
 }else{
   console.log("Expected the name to be Susan")
 }
