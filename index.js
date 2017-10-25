de68e4183a3df9ece5c2f38c1fc6de4fafcec481
Vi Nguyen <nguyenvi.8121@gmail.com>
2017-10-25 21:34:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 var name = "Susan"
 var height = "74"
-
+var index = require("./index.js")
 
 // Don't worry about this
 module.exports = { name, height
 }
 
 if (name === "Susan") {
-  console.log("The name is correct")
+  console.log("index.name")
 }else{
   console.log("Expected the name to be Susan")
 }
